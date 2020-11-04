31eb0a289ff19ed9d353d03a591e1dd6312ee718
Remy Ghalayini <remyghalayini@gmail.com>
2020-11-04 14:51:26 +0100

    first commit

---
+++ Lab_Interfaces/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonInfo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+
+            int age = int.Parse(Console.ReadLine());
+
+            IPerson person = new Citizen(name, age);
+            Console.WriteLine(person.Name);
+            Console.WriteLine(person.Age);
+            Console.ReadLine();
+        }
+    }
+}
